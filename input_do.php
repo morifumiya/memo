bef163b4c1d4e45ece5d30fcb7f903296a80b4f4
morifumiya <morimorimorifu@gmail.com>
2020-03-14 15:50:33 +0900

    fix:インデントの調整, 上ちゅな記述の削除

--- input_do.php
+++ input_do.php
@@ -1,25 +1,27 @@
 <!doctype html>
 <html lang="ja">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="css/style.css">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>PHP</title>
-    </head>
-    <body>
-        <header>
-            <h1 class="font-weight-normal">PHP</h1>    
-        </header>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.css">
 
-        <main>
-            <h2>Practice</h2>
-            <pre>
+    <title>PHP</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="font-weight-normal">PHP</h1>
+    </header>
+
+    <main>
+        <h2>Practice</h2>
+        <pre>
                 <?php
-                require('dbconnect.php'); 
+                require('dbconnect.php');
 
                 $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
                 $statement->bindParam(1, $_POST['memo']);
@@ -27,6 +29,7 @@
                 echo 'メッセージが登録されました';
                 ?>
             </pre>
-        </main>
-    </body>    
+    </main>
+</body>
+
 </html>
--- memo.php
+++ memo.php
@@ -1,45 +1,48 @@
 <!doctype html>
 <html lang="ja">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="css/style.css">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>PHP</title>
-    </head>
-    <body>
-        <header>
-            <h1 class="font-weight-normal">PHP</h1>    
-        </header>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.css">
 
-        <main>
-            <h2>Practice</h2>
-            <?php
-            require('dbconnect.php');
+    <title>PHP</title>
+</head>
 
-            $id = $_REQUEST['id'];
-            if (!is_numeric($id) || $id <= 0 ) {
-                print('1以上の数字を指定してください');
-                exit();
-            }
+<body>
+    <header>
+        <h1 class="font-weight-normal">PHP</h1>
+    </header>
 
-            $memos = $db->prepare('SELECT * FROM memos WHERE id=?');
-            $memos->execute(array($id));
-            $memo = $memos->fetch();
-            ?>
-            <article>
-                <pre><?php print($memo['memo']); ?></pre>
+    <main>
+        <h2>Practice</h2>
+        <?php
+        require('dbconnect.php');
 
-                <a href="update.php?id=<?php print($memo['id']); ?>">編集する</a>
-                |
-                <a href="delete.php?id=<?php print($memo['id']); ?>">削除する</a>
-                |
-                <a href="index.php">戻る</a>
-            </article>
+        $id = $_REQUEST['id'];
+        if (!is_numeric($id) || $id <= 0) {
+            print('1以上の数字を指定してください');
+            exit();
+        }
 
-        </main>
-    </body>    
+        $memos = $db->prepare('SELECT * FROM memos WHERE id=?');
+        $memos->execute(array($id));
+        $memo = $memos->fetch();
+        ?>
+        <article>
+            <pre><?php print($memo['memo']); ?></pre>
+
+            <a href="update.php?id=<?php print($memo['id']); ?>">編集する</a>
+            |
+            <a href="delete.php?id=<?php print($memo['id']); ?>">削除する</a>
+            |
+            <a href="index.php">戻る</a>
+        </article>
+
+    </main>
+</body>
+
 </html>
